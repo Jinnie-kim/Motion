33623ec5a537b757eea808528fafffa8fd00f1ce
Jinnie <hjk7639@gmail.com>
2024-11-11 19:38:50 +0900

    Fix: 사용자 입력 url애서 yutube id 뽑아내는 정규표현식 코드 & embed url 생성 코드 추가

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -10,10 +10,21 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
 
     const videoElement = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
 
-    videoElement.src = url; // url -> video Id
+    videoElement.src = this.converttoembededURl(url);
 
     const titleElement = this.element.querySelector('.video__title')! as HTMLParagraphElement;
 
     titleElement.textContent = title;
+  }
+
+  private converttoembededURl(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
