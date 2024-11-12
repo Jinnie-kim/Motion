3d5eb331b78ee5c8e8a6997e55983199c046509f
Jinnie <hjk7639@gmail.com>
2024-11-12 22:37:42 +0900

    Add: App 컴포넌트에 컴포넌트 Class 붙여넣기

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
+import { Component } from './components/component.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
+
+    const video = new VideoComponent('브람스를 좋아합니다.', 'https://www.youtube.com/watch?v=lysguDnH7uI');
+    this.page.addChild(video);
+
+    const note = new NoteComponent('This is note title', 'This is note body');
+    this.page.addChild(note);
+
+    const todo = new TodoComponent('오늘 할 일', '드림코딩 실전 프로젝트 완성하기');
+    this.page.addChild(todo);
   }
 }
 
