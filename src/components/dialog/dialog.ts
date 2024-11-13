27231958b2ee801d5d66ee3e45e1e63b72299810
Jinnie <hjk7639@gmail.com>
2024-11-13 21:45:10 +0900

    Refactor: 전체 코드 리팩토링

--- src/components/dialog/dialog.ts
+++ src/components/dialog/dialog.ts
@@ -4,6 +4,15 @@ import { Composable } from '../page/page.js';
 type OnCloseListener = () => void;
 type onSubmitListener = () => void;
 
+export interface MediaData {
+  readonly title: string;
+  readonly url: string;
+}
+
+export interface TextData {
+  readonly title: string;
+  readonly body: string;
+}
 export class InputDialog extends BaseComponent<HTMLElement> implements Composable {
   closeListener?: OnCloseListener;
   submitListener?: onSubmitListener;
